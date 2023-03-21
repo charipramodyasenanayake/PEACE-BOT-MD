e1f2f0a03b35211378eea87a97fa35cd4c35fba6
Charith Pramodya Senanayake <111125232+charipramodyasenanayake@users.noreply.github.com>
2023-03-21 06:48:18 -0700

    ᴘᴇᴀᴄᴇ ᴍᴅ ʙʏ ᴄʜᴀʀɪᴛʜ

---
+++ Commands/NSFW/wings.js
@@ -0,0 +1,42 @@
+const axios = require("axios");
+
+module.exports = {
+  name: "wings",
+  alias: ["nsfwwings"],
+  desc: "Hentai picture of waifu with wings", 
+  category: "Nsfw",
+  usage: `wings`,
+  react: "🍁",
+  start: async (Miku, m, { prefix,NSFWstatus }) => {
+
+    if (NSFWstatus == "false") return m.reply(`This group is not NSFW enabled!\n\nTo configure NSFW mode, type:\n\n*${prefix}nsfw*`);
+    m.reply(mess.waiting)
+    let buff= await axios.get(`https://fantox-apis.vercel.app/wings`)
+    let imgURL = buff.data.url
+    
+
+    let Button = [
+      {
+        buttonId: `${prefix}nsfwmenu`,
+        buttonText: { displayText: `NSFW Menu` },
+        type: 1,
+      },
+      {
+        buttonId: `${prefix}wings`,
+        buttonText: { displayText: `>>` },
+        type: 1,
+      },
+    ];
+    let bmffg = {
+      image: {url: imgURL},
+      caption: `PEACE MD BY CHARITH`,
+      footer: `ᴘᴇᴀᴄᴇ ᴍᴅ`,
+      buttons: Button,
+      headerType: 4,
+    };
+    
+    await Miku.sendMessage(m.from, bmffg, { quoted: m }).catch((err) => {
+      return "Error!";
+    });
+  },
+};
